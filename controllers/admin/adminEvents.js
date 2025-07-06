626cd624b82ae51c99a6019c02cc6f9b1850d6b1
daksh <dakshchaudhary584@gmail.com>
2025-07-06 22:58:54 +0530

    create page added

--- controllers/admin/adminEvents.js
+++ controllers/admin/adminEvents.js
@@ -4,6 +4,29 @@ function homePage(req,res){
     })
 }
 
+function createPage(req,res){
+    res.render("admin/events/create",{
+        title : "Admin - Create Event",
+        errorMessage: {},
+    })
+}
+
+function storePage(req,res){
+    res.render("admin/events/create",{
+        title : "Admin - Create Event",
+        errorMessage: {
+            title: "Title is required",
+            shortDescription: "Short description is required",
+            longDescription: "Long description is required",
+            pic: "Picture is required",
+            date: "Date is required",
+        }
+    })
+}
+
+
 module.exports = {
     homePage: homePage,
+    createPage: createPage,
+    storePage: storePage
 }
--- routes/admin/events.js
+++ routes/admin/events.js
@@ -2,8 +2,12 @@ const EventsRouter = require("express").Router();
 
 const {
     homePage,
+    createPage,
+    storePage,
 } = require("../../controllers/admin/adminEvents")
 
 EventsRouter.get("", homePage)
+EventsRouter.get("/create", createPage)
+EventsRouter.post("/store", storePage)
 
 module.exports = EventsRouter;
